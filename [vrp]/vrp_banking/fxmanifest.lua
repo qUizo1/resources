50335926ac97102207cc835de0169a7fd0f6db0f
qUizo-Dev <itzzquizo@gmail.com>
2025-11-02 01:36:50 +0200

    (banking): integrate vrp banking module
    Introduces a new banking resource to the vrp framework, enabling financial transactions and account management features for players. This addition expands the server's economic system without altering existing functionalities.

---
+++ [vrp]/vrp_banking/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_script {
+  "@vrp/lib/utils.lua"
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    "server.lua",
+}
+
+ui_page 'index/index.html'
+
+files {
+    'index/index.html',
+    'index/*.css',
+    'index/*.png',
+    'index/*.js'
+}
